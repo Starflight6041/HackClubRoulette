040ebdbb8eeb34996b72e4e3430c3c0eef0a472b
Starflight6041 <electricguitar123456@gmail.com>
2025-08-11 12:57:36 -0400

    Update Goblin.cs

--- Assets/Goblin.cs
+++ Assets/Goblin.cs
@@ -5,7 +5,10 @@ public class Goblin : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("test test test test test test test");
+        Debug.Log("Hack club please please please track my time :( :( :( :) :( :( :( :( :( :( ))))))))");
+        Debug.Log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+        Debug.Log("Surely it's been 1 minute by now aaaaaaaaaaaaaaaaaaaaaaaaaaaa");
     }
 
     // Update is called once per frame
