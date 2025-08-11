9d23f55d339b4aa7ced4dbea0b7a116b22b5a7e7
Starflight6041 <electricguitar123456@gmail.com>
2025-08-11 12:33:08 -0400

    Update Enemy.cs

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -7,7 +7,7 @@ public class Enemy : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Debug.Log("aaaaaa");
     }
 
     // Update is called once per frame
